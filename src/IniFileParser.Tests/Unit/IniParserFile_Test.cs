8336957434c3a8351cd2a90c43c66468be599b96
Juan José Montiel Pérez <jmontiel@pasiona.com>
2018-12-23 02:49:29 +0100

    MIgrate to NetStandard 2.0 Fixed tests after updating to latest NUnit engine.

--- src/IniFileParser.Tests/Unit/IniParserFile_Test.cs
+++ src/IniFileParser.Tests/Unit/IniParserFile_Test.cs
@@ -6,82 +6,79 @@ using IniParser;
 
 namespace IniFileParser.Tests.Unit
 {
-    [TestFixture, Category("File loading/saving tests")]
-    public class IniParserFileTest
-    {
+	[TestFixture, Category("File loading/saving tests")]
+	public class IniParserFileTest
+	{
 
-        public readonly string strGoodINIFilePath = @"../../INIFileGOOD.ini";
-        public readonly string strBadINIFilePath = @"../../INIfileBAD.ini";
-        public readonly string strEmptyINIFilePath = @"../../INIFileEMPTY.ini";
-        public readonly string strBadSectionINIFilePath = @"../../INIFileBADSection.ini";
-        public readonly string strBadKeysINIFilePath = @"../../INIfileBADKeys.ini";
+		public readonly string strGoodINIFilePath = @"INIFileGOOD.ini";
+		public readonly string strBadINIFilePath = @"INIfileBAD.ini";
+		public readonly string strEmptyINIFilePath = @"INIFileEMPTY.ini";
+		public readonly string strBadSectionINIFilePath = @"INIFileBADSection.ini";
+		public readonly string strBadKeysINIFilePath = @"INIfileBADKeys.ini";
 
-        public FileIniDataParser iniParser = new FileIniDataParser();
+		public FileIniDataParser iniParser = new FileIniDataParser();
 
-        [SetUp]
-        public void SetUp()
-        {
-        }
+		[SetUp]
+		public void SetUp()
+		{
+		}
 
-        [TearDown]
-        public void TearDown()
-        {
-        }
+		[TearDown]
+		public void TearDown()
+		{
+		}
 
-        [Test, Description("Checks existence of test INI files")]
-        public void CheckTestFilesExists()
-        {
-            Assert.That(File.Exists(strGoodINIFilePath), "Correct INI file {0} does not exists", strGoodINIFilePath);
-            Assert.That(File.Exists(strBadINIFilePath), "Invalid INI file {0} does not exists", strBadINIFilePath);
-            Assert.That(File.Exists(strEmptyINIFilePath), "Empty INI file {0} does not exists", strEmptyINIFilePath);
-        }
+		[Test, Description("Checks existence of test INI files")]
+		public void CheckTestFilesExists()
+		{
+			Assert.That(File.Exists(strGoodINIFilePath), "Correct INI file {0} does not exists", strGoodINIFilePath);
+			Assert.That(File.Exists(strBadINIFilePath), "Invalid INI file {0} does not exists", strBadINIFilePath);
+			Assert.That(File.Exists(strEmptyINIFilePath), "Empty INI file {0} does not exists", strEmptyINIFilePath);
+		}
 
-        [Test, Description("Checks correct parsing of an empty INI file")]
-        public void CheckParseEmptyFileSuccess()
-        {
-            IniData parsedData = iniParser.ReadFile(strEmptyINIFilePath);
+		[Test, Description("Checks correct parsing of an empty INI file")]
+		public void CheckParseEmptyFileSuccess()
+		{
+			IniData parsedData = iniParser.ReadFile(strEmptyINIFilePath);
 
-            Assert.That(parsedData, Is.Not.Null);
-        }
+			Assert.That(parsedData, Is.Not.Null);
+		}
 
-        [Test, Description("Checks correct parsing of a well formed INI file")]
-        public void CheckParseGoodFileSuccess()
-        {
-            IniData parsedData = iniParser.ReadFile(strGoodINIFilePath);
+		[Test, Description("Checks correct parsing of a well formed INI file")]
+		public void CheckParseGoodFileSuccess()
+		{
+			IniData parsedData = iniParser.ReadFile(strGoodINIFilePath);
 
-            Assert.That(parsedData, Is.Not.Null);
-        }
+			Assert.That(parsedData, Is.Not.Null);
+		}
 
-        [Test, Description("Checks error when parsing a bad formed INI file")]
-        [ExpectedException(typeof(ParsingException))]
-        public void CheckParsingFailure()
-        {
-            iniParser.ReadFile(strBadINIFilePath);
-        }
+		[Test, Description("Checks error when parsing a bad formed INI file")]
+		public void CheckParsingFailure()
+		{
+			Assert.That(() => iniParser.ReadFile(strBadINIFilePath), Throws.InstanceOf<ParsingException>());
+		}
 
-        [Test, Description("Checks correct saving of a file")]
-        public void CheckCorrectSave()
-        {
-            string fileString = strGoodINIFilePath + "_test.ini";
+		[Test, Description("Checks correct saving of a file")]
+		public void CheckCorrectSave()
+		{
+			string fileString = strGoodINIFilePath + "_test.ini";
 
-            IniData parsedData = iniParser.ReadFile(strGoodINIFilePath);
-            iniParser.WriteFile(fileString, parsedData);
+			IniData parsedData = iniParser.ReadFile(strGoodINIFilePath);
+			iniParser.WriteFile(fileString, parsedData);
 
-            Assert.That(File.Exists(fileString));
-        }
+			Assert.That(File.Exists(fileString));
+		}
 
-        [Test, Description("Checks bad formed INI file: Two sections with same name")]
-        [ExpectedException(typeof(ParsingException))]
-        public void CheckCollideSectionNames()
-        {
-            iniParser.ReadFile(strBadSectionINIFilePath);
-        }
+		[Test, Description("Checks bad formed INI file: Two sections with same name")]
+		public void CheckCollideSectionNames()
+		{
+			Assert.That(() => iniParser.ReadFile(strBadSectionINIFilePath), Throws.InstanceOf<ParsingException>());
+		}
 
-        [Test, Description("Checks bad formed INI file: Two keys in the same section with same name")]
-        [ExpectedException(typeof(ParsingException))]
-        public void CheckCollideKeysNames()
-        {
-            iniParser.ReadFile(strBadKeysINIFilePath);
-        }
-    }
+		[Test, Description("Checks bad formed INI file: Two keys in the same section with same name")]
+		public void CheckCollideKeysNames()
+		{
+			Assert.That(() => iniParser.ReadFile(strBadKeysINIFilePath), Throws.InstanceOf<ParsingException>());
+		}
+	}
 }
